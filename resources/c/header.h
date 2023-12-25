9b71e8554f42b079a7c922896583c5781f4b764f
Dennis Ostermann <dennis.ostermann@smail.th-koeln.de>
2023-12-25 04:22:20 +0100

    Fix enum declaration in header file

--- resources/c/header.h
+++ resources/c/header.h
@@ -14,7 +14,7 @@ struct SMOutput_/*CODE:NAME*/
 /*CODE:INPUTS_DECLARATION*/
 };
 
-// enum states state_/*CODE:NAME*/ = /*CODE:INITIAL_STATE*/;
+enum states state_/*CODE:NAME*/ = /*CODE:INITIAL_STATE*/;
 
 void sm_/*CODE:NAME*/ ( enum sm_states_/*CODE:NAME*/ *state,
                         struct SMInput_/*CODE:NAME*/ input,
