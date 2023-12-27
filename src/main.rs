9bfc22ad8986e7c4d0cf0d37252c469008f7417c
Dennis Ostermann <dennis.ostermann@smail.th-koeln.de>
2023-12-27 01:50:45 +0100

    Refactor parse_yaml function to return a Result

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let name = cli.name;
 
     // Parse the YAML file.
-    let state_machine = parse_yaml(&yaml_str);
+    let state_machine = parse_yaml(&yaml_str)?;
 
     // Check if the elements in the config are unique.
     match &state_machine {
@@ -36,25 +36,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn parse_yaml(yaml_str: &str) -> StateMachine {
+fn parse_yaml(yaml_str: &str) -> Result<StateMachine, &'static str> {
     let moore_machine: Result<MooreMachine, _> = serde_yaml::from_str(&yaml_str);
     let mealy_machine: Result<MealyMachine, _> = serde_yaml::from_str(&yaml_str);
 
     // Check if the YAML file contains a Moore or a Mealy machine.
-    let state_machine = match (moore_machine, mealy_machine) {
-        (Ok(moore_machine), Err(_)) => StateMachine::Moore(moore_machine),
-        (Err(_), Ok(mealy_machine)) => StateMachine::Mealy(mealy_machine),
-        (Err(e1), Err(e2)) => {
-            eprintln!("Error parsing YAML (moore): {:?}", e1);
-            eprintln!("Error parsing YAML (mealy): {:?}", e2);
-            process::exit(1);
-        }
-        (Ok(_), Ok(_)) => {
-            eprintln!("Error parsing YAML: Both Moore and Mealy machines found.");
-            process::exit(1);
-        }
-    };
-    state_machine
+    match (moore_machine, mealy_machine) {
+        (Ok(moore_machine), Err(_)) => Ok(StateMachine::Moore(moore_machine)),
+        (Err(_), Ok(mealy_machine)) => Ok(StateMachine::Mealy(mealy_machine)),
+        (Err(_), Err(_)) => Err(
+            "The YAML file does not contain a Moore or a Mealy machine. \
+                Please check the syntax of the file.",
+        ),
+        (Ok(_), Ok(_)) => Err("The YAML file contains both a Moore and a Mealy machine."),
+    }
 }
 
 #[cfg(test)]
@@ -80,7 +75,7 @@ mod test {
     #[test]
     fn test_config_moore() {
         let yaml_str = include_str!("../resources/test_moore.yaml");
-        let state_machine = parse_yaml(&yaml_str);
+        let state_machine = parse_yaml(&yaml_str).unwrap();
 
         let val = match state_machine {
             StateMachine::Moore(m) => check_if_config_elements_unique(&m),
